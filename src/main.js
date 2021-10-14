6f6e9008cb44a987ec36d982bbcb95404f1372d0
2465050990 <2465050990@qq.com>
2021-10-14 17:25:44 +0800

    搭建项目架构-初始目录调整

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入 vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 浏览器适配：引入 lib-flexible ⽤于设置 rem 基准值。
+// postcss-pxtorem 是⼀款 postcss 插件，⽤于将单位转化为 rem
+import 'amfe-flexible'
+
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
